d3650ea19e07b06c4cf4c821b7d66428c3b4c577
ricwork <ricardonwork@gmail.com>
2021-05-03 11:39:00 +0700

    update login.jsx

--- src/page/login.jsx
+++ src/page/login.jsx
@@ -1,21 +1,34 @@
 import React from "react"
 import {
-  Link
+  Link,
+  Redirect
 } from "react-router-dom"
+
+import Spotify from '../spotify.js'
+import {clientID, secretID} from '../env.js'
 
 class Login extends React.Component{
 	constructor(props)
 	{
 		super(props)
-		this.login = this.login.bind()
+		this.state = {
+			hasLogged: false
+		}
+		this.login = this.login.bind(this)
 	}
 	login(e)
 	{
 		e.preventDefault()
-		
+		var api = new Spotify(clientID, secretID)
+		api.getToken()
+		this.setState({hasLogged: true})
 	}
 	render()
 	{
+		if(this.state.hasLogged)
+		{
+			return <Redirect to="/"/>
+		}
 		return(
 			<div>
 				<section>
@@ -23,9 +36,15 @@ class Login extends React.Component{
 					<div className="flex text-center">
 						<div className="w-full sm:w-2/3 md:w-1/2 mx-auto">
 							<ul className="block">
-								<li className="flex"><a href="#" className="w-full rounded-2xl mt-5 bg-blue-900 hover:bg-blue-800 text-white p-4 font-bold">CONTINUE WITH FACEBOOK</a></li>
-								<li className="flex"><a href="#" className="w-full rounded-2xl mt-5 bg-black hover:bg-gray-900 text-white p-4 font-bold">CONTINUE WITH APPLE</a></li>
-								<li className="flex"><a href="#" className="w-full rounded-2xl mt-5 bg-white hover:bg-gray-100 text-black p-4 font-bold">CONTINUE WITH GOOGLE</a></li>
+								<li className="flex">
+									<a href="/" className="w-full rounded-2xl mt-5 bg-blue-900 hover:bg-blue-800 text-white p-4 font-bold">CONTINUE WITH FACEBOOK</a>
+								</li>
+								<li className="flex">
+									<a href="/" className="w-full rounded-2xl mt-5 bg-black hover:bg-gray-900 text-white p-4 font-bold">CONTINUE WITH APPLE</a>
+								</li>
+								<li className="flex">
+									<a href="/" className="w-full rounded-2xl mt-5 bg-white hover:bg-gray-100 text-black p-4 font-bold">CONTINUE WITH GOOGLE</a>
+								</li>
 							</ul>
 							<p className="mt-5 flex"><hr className="border-gray-300 mt-5 mb-5 w-2/4 mr-5"/> Or <hr className="border-gray-300 mt-5 mb-5 w-2/4 ml-5"/></p>
 							<form onSubmit={this.login}>
@@ -51,7 +70,7 @@ class Login extends React.Component{
 									</div>
 								</div>
 								<div className="block mt-3 text-left hover:underline">
-									<a href="">Forget Password</a>
+									<Link href="">Forget Password</Link>
 								</div>
 								<div className="sm:flex mt-7">
 									<div className="w-full sm:w-1/2">
